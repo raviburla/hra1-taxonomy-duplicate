dcb91b25231ea462f7f3e3a44321843f04850238
Ravi Burla <ravi.burla@autodesk.com>
2017-04-19 16:24:34 -0400

    Updates for the sprint

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,15 @@
 var RetiredForagerBee = function() {
+    Bee.call(ForagerBee);
+    this.age = 40;
+    this.job = "gamble";
+    this.color = "grey";
+    this.canFly = false;
+    this.food = "jelly";
+    this.treasureChest = [];
 };
 
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+
+RetiredForagerBee.prototype.forage = function () {return "I am too old, let me play cards instead"};
+RetiredForagerBee.prototype.gamble = function (treasure) {this.treasureChest.push(treasure)};
